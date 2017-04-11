8e20afce6dac26f9bada070e5c7c1eba25dffdfc
Fela Pyne <opyne@wlapcs.org>
2017-04-11 17:40:57 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+
+
+
+function setup(){
+createCanvas(700,500);
+background(0);
+}
+
+function draw(){
+    for(var x = 0; x <=600; x = x + 50){
+         strokeWeight(4);
+    stroke(255);
+    var x = 0;
+    ellipse(mouseX,mouseY,50,50);
+    fill(255,50,50)
+    ellipse(mouseX,mouseY)
+    
+    
+
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+ 
+}
